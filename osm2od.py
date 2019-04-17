5c577e1272767d916ff4eaa15a459da8417dc8ec
Christoffer Wilhelm Gran <cwgran@gmail.com>
2019-04-17 17:20:47 +0200

    Add lanes to xml generation function Modify find_parallel to be suited for xml generation

--- osm2od.py
+++ osm2od.py
@@ -82,10 +82,7 @@ def buildXML(filename, roads):
     # TODO: Get CDATA working with ElementTree, or switch to lxml.etree
     #georef.text = "<![CDATA[+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs]]>"
 
-    print(len(roads))
     for r in roads:
-        pprint(vars(r))
-
         road = SubElement(root, "road")
 
         if hasattr(r, "name"):
@@ -104,6 +101,7 @@ def buildXML(filename, roads):
         roadgeo.set("y", str(r.nodes[0].lat))
         roadgeo.set("z", "0")
 
+        # Calculate road length
         length = 0
         for i in range(len(r.nodes)-1):
             p1 = r.nodes[i]
@@ -122,30 +120,75 @@ def buildXML(filename, roads):
             p.set("x", str(n.lng))
             p.set("y", str(n.lat))
             p.set("z", "0")
+
+        ##########
+        #### Lanes
+        ##########
+
+        lanes = SubElement(road, "lanes")
+
+        num_lanes = 1
+        lane_width = 3.0
+        if hasattr(r, "lanes"):
+            num_lanes = int(r.lanes)
+
+        laneSec = SubElement(lanes, "laneSection")
+        laneSec.set("singleSide", "true")          # True if both directions share the same laneSection
+
+        boundaries = SubElement(laneSec, "boundaries")
+
+        # If the lane number is odd and greater than 1, only care about lanes-1 lanes
+        if num_lanes > 1 and num_lanes % 2 != 0:
+            num_lanes -= 1
+
+        # Lane boundaries
+        left_boundary = SubElement(boundaries, "boundary")
+        left_boundary.set("type", "leftBoundary")
+
+        right_boundary = SubElement(boundaries, "boundary")
+        right_boundary.set("type", "rightBoundary")
+
+        # Lane boundary geometries
+        leftb_geo = SubElement(left_boundary, "geometry")
+        leftb_geo_ps = SubElement(leftb_geo, "pointSet")
+
+        rightb_geo = SubElement(right_boundary, "geometry")
+        rightb_geo_ps = SubElement(rightb_geo, "pointSet")
+        
+        boundary_width = num_lanes/2.0*lane_width
+        left_boundary_points = find_parallel(r.nodes, boundary_width, True)
+        right_boundary_points = find_parallel(r.nodes, boundary_width, False)
+
+        for i in range(len(r.nodes)):
+            # Left
+            lp = SubElement(leftb_geo_ps, "point")
+            lp.set("x", str(left_boundary_points[i][0]))
+            lp.set("y", str(left_boundary_points[i][1]))
+
+            # Right
+            rp = SubElement(rightb_geo_ps, "point")
+            rp.set("x", str(right_boundary_points[i][0]))
+            rp.set("y", str(right_boundary_points[i][1]))
+
+
         
     print("Done building, writing to '{}'".format(filename))
     tree.write(open('test.xml', 'w'), encoding='unicode')
 
-def find_borders(roads):
-    road = 0
-    road = roads[road]
-    pprint(vars(road))
+def find_parallel(road, width, left):
     points = []
 
     #for i in range(0, 3):
         #points.append((road.nodes[i].lng, road.nodes[i].lat))
 
-    print("Antall noder: {}".format(len(road.nodes)))
-    for n in road.nodes:
+    for n in road:
         points.append((n.lng, n.lat))
 
     points = np.array(points)
     vectors = []
 
     road_width = 5/2
-    border_left = []
-    border_right = []
-    print(points)
+    parallel = []
     for i in range(len(points)-1):
         p1 = points[i]
         p2 = points[i+1]
@@ -158,40 +201,21 @@ def find_borders(roads):
         # The relationship between the meter distance, and the vector norm
         a = vlen/np.linalg.norm(v)
         
-        # The two orthogonal vectors, scaled using a and the desired length as specified in road_width
+        # The two orthogonal vectors, scaled using a and the desired length as specified in width
         # Two new points are made by adding the vectors to p1
-        left = np.array([-v[1], v[0]])
-        l = (road_width/a)*left/np.linalg.norm(left)
+        lv = np.array([-v[1], v[0]]) if left else np.array([v[1], -v[0]])
+        l = (width/a)*lv/np.linalg.norm(lv)
         lp = (p1[0] + l[0], p1[1] + l[1])
-
-        right = np.array([v[1], -v[0]])
-        r = (road_width/a)*right/np.linalg.norm(right)
-        rp = (p1[0] + r[0], p1[1] + r[1])
-
-        border_left.append(lp)
-        border_right.append(rp)
+        
+        parallel.append(lp)
 
         # If this is the last iteration, add a point for the final point using the same orthogonal vectors as for n-1
         if i == len(points)-2:
             lp = (p2[0] + l[0], p2[1] + l[1])
-            rp = (p2[0] + r[0], p2[1] + r[1])
 
-            border_left.append(lp)
-            border_right.append(rp)
-
-    # A simple print of the calculated meter distance between the old and the generated points
-    print("Checking accuracy of generated points:")
-    for i in range(len(points)):
-        print("Point {}".format(i))
-        print("Left: {}".format(distance.distance(points[i], border_left[i]).m))
-        print("Right: {}".format(distance.distance(points[i], border_right[i]).m))
-        print("Left-right: {}".format(distance.distance(border_left[i], border_right[i]).m))
-
-    plt.plot(points, color="red")
-    plt.plot(border_left, color="green")
-    plt.plot(border_right, color="blue")
-    plt.show()
-        
+            parallel.append(lp)
+    
+    return parallel
 
 def main():
     parser = argparse.ArgumentParser()
@@ -204,8 +228,8 @@ def main():
         filename = args.file
 
     nodes, roads = readOSM(filename)
-    find_borders(roads)
-    #buildXML("test", roads)
+    #find_parallel(roads)
+    buildXML("test", roads)
 
 
 if __name__ == "__main__":
