1cc697389d4b880cfee71434939859fef4707da9
Christoffer Wilhelm Gran <cwgran@gmail.com>
2019-04-25 11:57:48 +0200

    Rewrite to lxml.etree

--- osm2od.py
+++ osm2od.py
@@ -2,21 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import datetime
-
-from xml.etree.ElementTree import ElementTree
-from xml.etree.ElementTree import Element
-from xml.etree.ElementTree import SubElement
-
-from xml.sax.saxutils import unescape
+import argparse
 
 from geopy import distance
 from scipy.optimize import curve_fit
-
-# For debugging
-from pprint import pprint
-
-import xml.etree.ElementTree as ET
-import argparse
+from lxml import etree
 
 from road import Node, Road
 
@@ -53,7 +43,7 @@ def readRoads(e, nodes):
     return roads
 
 def readOSM(filename):
-    e = ET.parse('osmmaps/{}'.format(filename)).getroot()
+    e = etree.parse('osmmaps/{}'.format(filename)).getroot()
 
     print("Reading file osmmaps/{}".format(filename))
 
@@ -67,18 +57,19 @@ def readOSM(filename):
 def format_coord(n):
     return "{:.9E}".format(n)
 
-def buildXML(filename, roads):
+def buildXML(filename, roads, pretty):
 
     name = filename.split(".")[0]
-    filename = name + ".xml"
+    #filename = name + ".xml"
+    filename = "base_map.xml"
     
     print("Building XML output...")
 
-    root = Element('OpenDRIVE')
-    tree = ElementTree(root)
+    root = etree.Element('OpenDRIVE')
+    tree = etree.ElementTree(root)
 
     # Setup header record
-    header = SubElement(root, "header")
+    header = etree.SubElement(root, "header")
     header.set("revMajor", "1")
     header.set("revMinor", "0")
     header.set("vendor", "Baidu")
@@ -91,12 +82,12 @@ def buildXML(filename, roads):
     max_coord = [None,None,None,None]
 
     # Setup Geo Reference
-    georef = SubElement(header, "geoReference")
+    georef = etree.SubElement(header, "geoReference")
     # TODO: Get CDATA working with ElementTree, or switch to lxml.etree
-    #georef.text = "<![CDATA[+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs]]>"
+    georef.text = etree.CDATA("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
     for r in roads:
-        road = SubElement(root, "road")
+        road = etree.SubElement(root, "road")
 
         if hasattr(r, "name"):
             road.set("name", r.name)
@@ -134,35 +125,35 @@ def buildXML(filename, roads):
         #### Lanes
         ##########
 
-        lanes = SubElement(road, "lanes")
+        lanes = etree.SubElement(road, "lanes")
 
         num_lanes = 1
         lane_width = 3.0
         if hasattr(r, "lanes"):
             num_lanes = int(r.lanes)
 
-        laneSec = SubElement(lanes, "laneSection")
+        laneSec = etree.SubElement(lanes, "laneSection")
         laneSec.set("singleSide", "true")          # True if both directions share the same laneSection
 
-        boundaries = SubElement(laneSec, "boundaries")
+        boundaries = etree.SubElement(laneSec, "boundaries")
 
         # If the lane number is odd and greater than 1, only care about num_lanes-1 lanes
         if num_lanes > 1 and num_lanes % 2 != 0:
             num_lanes -= 1
 
         # Lane boundaries
-        left_boundary = SubElement(boundaries, "boundary")
+        left_boundary = etree.SubElement(boundaries, "boundary")
         left_boundary.set("type", "leftBoundary")
 
-        right_boundary = SubElement(boundaries, "boundary")
+        right_boundary = etree.SubElement(boundaries, "boundary")
         right_boundary.set("type", "rightBoundary")
 
         # Lane boundary geometries
-        leftb_geo = SubElement(left_boundary, "geometry")
-        leftb_geo_ps = SubElement(leftb_geo, "pointSet")
+        leftb_geo = etree.SubElement(left_boundary, "geometry")
+        leftb_geo_ps = etree.SubElement(leftb_geo, "pointSet")
 
-        rightb_geo = SubElement(right_boundary, "geometry")
-        rightb_geo_ps = SubElement(rightb_geo, "pointSet")
+        rightb_geo = etree.SubElement(right_boundary, "geometry")
+        rightb_geo_ps = etree.SubElement(rightb_geo, "pointSet")
         
         boundary_width = num_lanes/2.0*lane_width
         left_boundary_points = find_parallel(r.nodes, boundary_width, True)
@@ -170,13 +161,13 @@ def buildXML(filename, roads):
 
         for i in range(len(r.nodes)):
             # Left
-            lp = SubElement(leftb_geo_ps, "point")
+            lp = etree.SubElement(leftb_geo_ps, "point")
             lp.set("x", format_coord(left_boundary_points[i][0]))
             lp.set("y", format_coord(left_boundary_points[i][1]))
             lp.set("z", format_coord(0.0))
 
             # Right
-            rp = SubElement(rightb_geo_ps, "point")
+            rp = etree.SubElement(rightb_geo_ps, "point")
             rp.set("x", format_coord(right_boundary_points[i][0]))
             rp.set("y", format_coord(right_boundary_points[i][1]))
             rp.set("z", format_coord(0.0))
@@ -184,8 +175,8 @@ def buildXML(filename, roads):
         # Center is supposed to store the reference line
         # Left/right stores the borders of left/right lanes
 
-        center = SubElement(laneSec, "center")
-        center_lane = SubElement(center, "lane")
+        center = etree.SubElement(laneSec, "center")
+        center_lane = etree.SubElement(center, "lane")
 
         center_lane.set("id", "0")
         center_lane.set("uid", "{}_0".format(r.id))
@@ -193,18 +184,18 @@ def buildXML(filename, roads):
         center_lane.set("direction", "bidirection")
         center_lane.set("turnType", "noTurn")   # Not sure what this means
 
-        center_line = SubElement(center_lane, "centerLine")
-        cl_geo = SubElement(center_line, "geometry")
+        center_line = etree.SubElement(center_lane, "centerLine")
+        cl_geo = etree.SubElement(center_line, "geometry")
         cl_geo.set("sOffset", "0")
         cl_geo.set("x", format_coord(r.nodes[0].lng))
         cl_geo.set("y", format_coord(r.nodes[0].lat))
         cl_geo.set("z", format_coord(0.0))
         cl_geo.set("length", str(road_length(r.nodes)))
 
-        cl_geo_ps = SubElement(cl_geo, "pointSet")
+        cl_geo_ps = etree.SubElement(cl_geo, "pointSet")
 
         for n in r.nodes:
-            p = SubElement(cl_geo_ps, "point")
+            p = etree.SubElement(cl_geo_ps, "point")
             p.set("x", format_coord(n.lng))
             p.set("y", format_coord(n.lat))
             p.set("z", format_coord(0.0))
@@ -226,64 +217,64 @@ def buildXML(filename, roads):
             if max_coord[3] == None or max_coord[3] > n.lng:
                 max_coord[3] = n.lng
 
-        right = SubElement(laneSec, "right")
+        right = etree.SubElement(laneSec, "right")
 
         if num_lanes > 1:
-            left = SubElement(laneSec, "left")
+            left = etree.SubElement(laneSec, "left")
 
         for i in range(math.ceil(num_lanes/2)):
             # Right, only add this if num_lanes == 1
-            right_lane = SubElement(right, "lane")
+            right_lane = etree.SubElement(right, "lane")
             right_lane.set("id", "-{}".format(i+1))
             right_lane.set("uid", "{}_1{}".format(r.id, i+1))
             right_lane.set("type", "driving")
             right_lane.set("direction", "bidirection" if num_lanes == 1 else "forward")
             right_lane.set("turnType", "noTurn")    # Not sure what this means
 
-            right_border = SubElement(right_lane, "border")
+            right_border = etree.SubElement(right_lane, "border")
             right_border.set("virtual", "TRUE")     # "Identify whether the lane boundary exists in real world"
 
             right_border_points = find_parallel(r.nodes, i*lane_width, True)
 
-            rb_geo = SubElement(right_border, "geometry")
+            rb_geo = etree.SubElement(right_border, "geometry")
             rb_geo.set("sOffset", "0")
             rb_geo.set("x", format_coord(right_border_points[0][0]))
             rb_geo.set("y", format_coord(right_border_points[0][1]))
             rb_geo.set("z", format_coord(0.0))
             rb_geo.set("length", str(road_length(right_border_points)))
 
-            rb_geo_ps = SubElement(rb_geo, "pointSet")
+            rb_geo_ps = etree.SubElement(rb_geo, "pointSet")
 
             for n in right_border_points:
-                p = SubElement(rb_geo_ps, "point")
+                p = etree.SubElement(rb_geo_ps, "point")
                 p.set("x", format_coord(n[0]))
                 p.set("y", format_coord(n[1]))
                 p.set("z", format_coord(0.0))
 
             if num_lanes > 1:
-                left_lane = SubElement(left, "lane")
+                left_lane = etree.SubElement(left, "lane")
                 left_lane.set("id", "{}".format(i+1))
                 left_lane.set("uid", "{}_0{}".format(r.id, i+1))
                 left_lane.set("type", "driving")
                 left_lane.set("direction", "backward")
                 left_lane.set("turnType", "noTurn")    # Not sure what this means
 
-                left_border = SubElement(left_lane, "border")
+                left_border = etree.SubElement(left_lane, "border")
                 left_border.set("virtual", "TRUE")     # "Identify whether the lane boundary exists in real world"
 
                 left_border_points = find_parallel(r.nodes, i*lane_width, False)
 
-                lb_geo = SubElement(left_border, "geometry")
+                lb_geo = etree.SubElement(left_border, "geometry")
                 lb_geo.set("sOffset", "0")
                 lb_geo.set("x", format_coord(left_border_points[0][0]))
                 lb_geo.set("y", format_coord(left_border_points[0][1]))
                 lb_geo.set("z", format_coord(0.0))
                 lb_geo.set("length", str(road_length(left_border_points)))
 
-                lb_geo_ps = SubElement(lb_geo, "pointSet")
+                lb_geo_ps = etree.SubElement(lb_geo, "pointSet")
 
                 for n in left_border_points:
-                    p = SubElement(lb_geo_ps, "point")
+                    p = etree.SubElement(lb_geo_ps, "point")
                     p.set("x", format_coord(n[0]))
                     p.set("y", format_coord(n[1]))
                     p.set("z", format_coord(0.0))
@@ -293,7 +284,8 @@ def buildXML(filename, roads):
     header.set("east", format_coord(max_coord[2]))
     header.set("west", format_coord(max_coord[3]))
     print("Done building, writing to '{}'".format(filename))
-    tree.write(open(filename, 'w'), encoding='unicode')
+
+    tree.write(filename, xml_declaration=True, pretty_print=pretty, encoding='UTF-8')
 
 # Calculate road length
 def road_length(road):
@@ -362,7 +354,7 @@ def main():
         filename = args.file
 
     nodes, roads = readOSM(filename)
-    buildXML(filename, roads)
+    buildXML(filename, roads, True)
 
 
 if __name__ == "__main__":
